9de286b3fdea693a135f4b594c5efb9f50b1e595
Gustavo Fraga <gustavohenriquemiele@live.com>
2020-12-23 09:02:38 -0300

    teste

--- src/config/worker.js
+++ src/config/worker.js
@@ -2,18 +2,19 @@ const { CronJob } = require('cron');
 const productsController = require('../controllers/productsController');
 
 console.log('running...')
-const jobDeleteData = new CronJob('25 30 23 * * *', async function() {
+const jobDeleteData = new CronJob('00 06 09 * * *', async function() {
     console.log('chegou job delete data')
     await productsController.deleteAllProducts()
     console.log('saiu job delete data')
 }, null, true, 'America/Sao_Paulo');
 
-const jobGetProducts = new CronJob('20 31 23 * * *', async function() {
+const jobGetProducts = new CronJob('30 06 09 * * *', async function() {
     console.log('chegou job get products')
     await productsController.getProductsTiny();
     await productsController.handleSendPlanilha()
     console.log('saiu job get products - done ya')
 }, null, true, 'America/Sao_Paulo');
+
 //'0 23 * * * *'
 //1  ->   '0 17 * * * *'   delete all
 //1  ->   '1 17 * * * *'   get products
